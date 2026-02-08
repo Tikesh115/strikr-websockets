072cf8088a39eb6837f358f12755ac41403975b3
Tikesh Sahu <tikeshs392@gmail.com>
2026-02-08 13:11:43 +0530

    implement arcjet security

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { matchRouter } from './routes/matches.js';
-
-const app = express();
+import http from 'http';
+import { attachWebSocketServer } from './ws/server.js';
+import { securityMiddleware} from './arcjet.js'
 
 // Define the port
-const PORT = 8000;
+const PORT = Number(process.env.PORT || 8000);
+const HOST = process.env.HOST || '0.0.0.0';
+
+const app = express();
+const server = http.createServer(app)
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -14,9 +19,17 @@ app.get('/', (req, res) => {
     res.send('Hello from Express server!');
 });
 
+app.use(securityMiddleware());
+
 app.use('/matches', matchRouter);
 
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+server.listen(PORT, () => {
+    const baseUrl = HOST === '0.0.0.0' ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+
+    console.log(`Server is running on ${baseUrl}`);
+    console.log(`WebSocket server running on ${baseUrl.replace('http', 'ws')}/ws`);
 });
